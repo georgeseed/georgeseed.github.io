fc8bd8c0bec78bacbc375722595f5dda07e3c781
George Seed <50909308+georgeseed@users.noreply.github.com>
2022-11-01 13:33:24 +0000

    Update dataScript.js

--- dataScript.js
+++ dataScript.js
@@ -1,4 +1,16 @@
-import fetchJSON from "./modules/dataLoader.js";
+async function fetchJSON(url, headers) {
+  //await keyword - call this function and expect a promise in return. Fetch returns a promise object that contains methods
+  //from the Response object (delegates, in Class terms, it's a constructor)
+    let response = await fetch(url,
+      {
+      headers: headers
+      });
+  //data = the result of the fetch() function, which here I've called 'response' and is
+  // a promise with some special methods for reading it from the Response object like .json() or .text()
+    let data = await response.json();
+    return data
+}
+
 function myFunction() {
 var auth = document.getElementById('auth').value;
 var request = document.getElementById('request').value;
